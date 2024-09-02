3bc7152f1a210f900c6b55397d9c259371dce5cc
PhotonPotato <photonpotato21@gmail.com>
2024-09-02 00:29:50 -1000

    v0.0.12 - Change Layout For Screen Size

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const translate = document.querySelectorAll(".translate");
+const gridContainers = document.querySelectorAll(".grid-template-based-on-screen-size");
+const order = document.querySelectorAll(".reorder-for-screen-size")
 
 window.addEventListener('scroll', () =>{
     let scroll = window.pageYOffset;
@@ -9,3 +11,46 @@ window.addEventListener('scroll', () =>{
         element.style.transform = `translateY(${scroll * speed}px)`;
     })
 })
+
+checkScreenSizeForGridReorder();
+
+window.addEventListener('resize', () => {
+    checkScreenSizeForGridReorder();
+})
+
+//Check to reorder and set grid layouts based on screen size
+function checkScreenSizeForGridReorder(){
+    if (window.innerWidth < 900){
+        switchToRows();
+        
+        order.forEach(element =>{
+            element.style.order = -1;
+        })
+    }else{
+        console.log("ordering2");
+
+        switchToColumns();
+        
+        order.forEach(element =>{
+            element.style.order = 1;
+        })
+    }
+}
+
+//Switch grid layouts to rows
+function switchToRows(){
+    gridContainers.forEach(element => {
+        element.style.gridTemplateColumns = 'none';
+        element.style.gridTemplateRows = '1fr 1fr';
+    })
+}
+
+//Switch grid layouts to columns
+function switchToColumns(){
+    console.log("switch to coluns");
+
+    gridContainers.forEach(element => {
+        element.style.gridTemplateColumns = '1fr 1fr';
+        element.style.gridTemplateRows = 'none';
+    })
+}
